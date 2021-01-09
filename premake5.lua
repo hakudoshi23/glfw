66e7972c9334b24fad85341c9a9dcde85f79704f
Aleix <aleixvalls14@gmail.com>
2021-01-09 17:07:52 +0100

    update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,8 @@ project "GLFW"
 	kind "StaticLib"
 	language "C"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
